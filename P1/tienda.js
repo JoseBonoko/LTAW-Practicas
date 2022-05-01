b093094a5d21d1e8ea842edbfc689a1dc92a2e27
JoseBonoko <jbonoko.jb@gmail.com>
2022-05-01 19:55:12 +0200

    Modificaciones y añadida nueva pagina

--- P1/tienda.js
+++ P1/tienda.js
@@ -32,25 +32,35 @@ const server = http.createServer((req, res) => {
     console.log("Recurso: " + FICHERO);
     console.log("Tipo de recurso: " + file_type);
 
+    const mime_type = {
+        'html': 'text/html',
+        'css': 'text/css',
+        'jpg': 'image/jpg',
+        'png': 'image/png',
+        'ico': 'image/x-icon'
+    };
+
+    let mime = mime_type[file_type];
+    console.log("Tipo de mime: " + mime);
 
     //-- Lectura del fichero
     fs.readFile(FICHERO, 'utf8', (err, data) => {
-        if (err){   //--Ha ocurrido algún error
+        if (err){   //--Ha ocurrido algún error, fichero no encontrado
             console.log("Error!!");
             code = 404;
             code_msg = 'Not Found';
             FICHERO = 'error.html';
+            data = fs.readFileSync(FICHERO);
             console.log(err.message);
         }else{  //--Lectura normal
             console.log("Lectura completada...");
             console.log("Contenido del fichero: \n");
             console.log(data);
-
         }
         //--Crear mensaje de respuesta
         res.statusCode = code;
         res.statusMessage = code_msg;
-        res.setHeader('Content-Type','text/html');
+        res.setHeader('Content-Type', mime);
         res.write(data);
         res.end()
     });
